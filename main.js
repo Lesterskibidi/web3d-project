a82bb8912972d0b5c8c3c3070ed63b8c3dd8c73e
Daniel <ngodaniel140@gmail.com>
2026-03-11 05:05:22 +0700

    fix path

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js'
 import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js'
 import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
-const MODEL_URL = "/hoshinoglb.glb";
-const AUDIO_URL = "/voice.mp3";
+const MODEL_URL = "public/hoshinoglb.glb";
+const AUDIO_URL = "public/voice.mp3";
 
 // ===== SCENE =====
 const scene = new THREE.Scene();
