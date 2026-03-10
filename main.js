5e721a94d837480487f65e4cf51fb7393bded6ae
Daniel <ngodaniel140@gmail.com>
2026-03-11 05:11:17 +0700

    fix three import

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js'
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
+import { GLTFLoader } from "https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
+
 import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
 
 const MODEL_URL = "public/hoshinoglb.glb";
